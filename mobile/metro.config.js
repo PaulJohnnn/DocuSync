c335d154b881314402b194e9791da11677d55d9b
Henturo <pauljohnpalamara123@gmail.com>
2026-06-15 09:32:12 +0800

    Upgrade mobile to Expo SDK 53 (Expo Go SDK 54 compatible) with exact peer versions

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,12 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
 const config = getDefaultConfig(__dirname);
-
-// Fix Windows ENOENT: Metro tries to watch iOS test folders that don't exist on Windows
-config.watchFolders = [];
-config.resolver.blockList = [
-  /.*\/iostests\/.*/,
-  /.*\\iostests\\.*/,
-];
-
 module.exports = config;
